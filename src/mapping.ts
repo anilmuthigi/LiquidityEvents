df4a0605edaeb7f3790690781b726517d9f4672a
anilmuthigi <54994968+anilmuthigi@users.noreply.github.com>
2022-02-18 02:56:54 +0530

    Initial Commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,50 +1,12 @@
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  Collect as CollectEvent,
   DecreaseLiquidity as DecreaseLiquidityEvent,
-  IncreaseLiquidity as IncreaseLiquidityEvent,
-  Transfer as TransferEvent
+  IncreaseLiquidity as IncreaseLiquidityEvent
 } from "../generated/LiquidityEvents/LiquidityEvents"
 import {
-  Approval,
-  ApprovalForAll,
-  Collect,
   DecreaseLiquidity,
-  IncreaseLiquidity,
-  Transfer
+  IncreaseLiquidity
 } from "../generated/schema"
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-  entity.save()
-}
-
-export function handleCollect(event: CollectEvent): void {
-  let entity = new Collect(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.tokenId = event.params.tokenId
-  entity.recipient = event.params.recipient
-  entity.amount0 = event.params.amount0
-  entity.amount1 = event.params.amount1
-  entity.save()
-}
 
 export function handleDecreaseLiquidity(event: DecreaseLiquidityEvent): void {
   let entity = new DecreaseLiquidity(
@@ -68,12 +30,3 @@ export function handleIncreaseLiquidity(event: IncreaseLiquidityEvent): void {
   entity.save()
 }
 
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-  entity.save()
-}
